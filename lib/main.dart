2958d02029051a893b727eaa844782747f084164
bngftm <2499610321@qq.com>
2022-06-12 21:02:46 +0800

    新增

--- lib/main.dart
+++ lib/main.dart
@@ -133,7 +133,54 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ],
             ),
-          
+            Flex(
+              direction: Axis.horizontal,
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    height: 30.0,
+                    color: Colors.red,
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    height: 30.0,
+                    color: Colors.green,
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: SizedBox(
+                height: 100.0,
+                //Flex的三个子widget，在垂直方向按2：1：1来占用100像素的空间  
+                child: Flex(
+                  direction: Axis.vertical,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        height: 30.0,
+                        color: Colors.red,
+                      ),
+                    ),
+                    Spacer(
+                      flex: 1,
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        height: 30.0,
+                        color: Colors.green,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
             Wrap(
               spacing: 8.0, // 主轴(水平)方向间距
               runSpacing: 4.0, // 纵轴（垂直）方向间距
@@ -156,6 +203,14 @@ class _MyHomePageState extends State<MyHomePage> {
                   label: Text('Laurens'),
                 ),
               ],
+            ),
+            Align(
+              widthFactor: 2,
+              heightFactor: 2,
+              alignment: Alignment.topRight,
+              child: FlutterLogo(
+                size: 60,
+              ),
             ),
           ],
         ),
